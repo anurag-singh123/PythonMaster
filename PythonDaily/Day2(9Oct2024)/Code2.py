2e42d4573b0dca146e489ca65c1241818e0da1fd
Anurag Singh <anuragsinghchauhan456@gmail.com>
2024-10-09 09:24:58 +0530

    Code2 is Pushed

--- PythonDaily/Day2(9Oct2024)/Code2.py
+++ PythonDaily/Day2(9Oct2024)/Code2.py
@@ -1,2 +1,8 @@
 # Write a Python program that takes a list of numbers and sorts them in ascending order.
 
+my_list = [int(x) for x in input("Enter a list of numbers separated by space: ").split()]
+print("Original List:",my_list)
+
+my_list = sorted(my_list)
+
+print("Sorted List:",my_list)
