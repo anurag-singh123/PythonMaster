f39e31794d36ad6fad1d55a916b330b30bd7e0e4
Anurag Singh <anuragsinghchauhan456@gmail.com>
2024-10-14 22:28:47 +0530

    Code1 pushed today

---
+++ PythonDaily/Day7(14Oct2024)/Code1.py
@@ -0,0 +1,5 @@
+#Write a python program to print first 25 odd numbers using range()
+
+for i in range(1,25+1):
+    if i%2!=0:
+        print(i)
---
+++ PythonDaily/Day7(14Oct2024)/Code2.py
@@ -0,0 +1,2 @@
+#Write a program to count the number of alphabets and numbers of digits in the string 'Nagpur-440010'
+
