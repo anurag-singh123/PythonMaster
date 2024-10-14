3e79dc7858f82c1bdf5df8610ff7c8527befc5f2
Anurag Singh <anuragsinghchauhan456@gmail.com>
2024-10-14 22:38:54 +0530

    Code2 pushed

--- PythonDaily/Day7(14Oct2024)/Code2.py
+++ PythonDaily/Day7(14Oct2024)/Code2.py
@@ -1,2 +1,14 @@
 #Write a program to count the number of alphabets and numbers of digits in the string 'Nagpur-440010'
 
+my_string='Nagpur-440010'
+
+alphabet_count=0
+digit_count=0
+for char in my_string:
+    if char.isalpha():
+        alphabet_count += 1
+    elif char.isdigit():
+        digit_count += 1
+
+print("Alaphabets:",alphabet_count)
+print("Digits:",digit_count)
